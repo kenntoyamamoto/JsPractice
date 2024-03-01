65573ed71df912ba499f5f0ec636e599a7babb32
kenntoyamamoto <yamakenn5598@gmail.com>
2024-03-01 15:05:40 +0900

    イベント発火　基礎編

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+let pullDownButton = document.getElementById("lists");
+pullDownButton.addEventListener("mouseover", function () {
+  pullDownButton.setAttribute("style", "background-color:blue;");
+});
 
-const pullDownButton=document.getElementById('lists');
-window.addEventListener("mouseover",function () {
-  console.log(pullDownButton);
-})
+pullDownButton.addEventListener("mouseout", function () {
+  pullDownButton.removeAttribute("style", "background-color:red;");
+});
+
+pullDownButton.addEventListener("click", function () {
+  pullDownButton.setAttribute("style", "background-color:green;");
+});
+
+pullDownButton.addEventListener("mouseover", function () {
+  console.log("乗った時は青色");
+});
+
+pullDownButton.addEventListener("mouseout", function () {
+  console.log("外れた時は赤色");
+});
+
+pullDownButton.addEventListener("click", function () {
+  console.log("クリックは緑色");
+});
