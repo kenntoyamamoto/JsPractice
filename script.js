64debcdcfcba5d616d3bc8558d2e317ca4f8fbd6
kenntoyamamoto <yamakenn5598@gmail.com>
2024-03-04 07:54:36 +0900

    条件分岐編
    条件分岐編

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
+
 let pullDownButton = document.getElementById("lists");
-pullDownButton.addEventListener("mouseover", function () {
-  pullDownButton.setAttribute("style", "background-color:blue;");
-});
-
-pullDownButton.addEventListener("mouseout", function () {
-  pullDownButton.removeAttribute("style", "background-color:red;");
-});
-
-pullDownButton.addEventListener("click", function () {
-  pullDownButton.setAttribute("style", "background-color:green;");
-});
+let pullDownParents = document.getElementById("pull-down");
 
 pullDownButton.addEventListener("mouseover", function () {
+  this.style.backgroundColor = "blue";
   console.log("乗った時は青色");
 });
 
 pullDownButton.addEventListener("mouseout", function () {
+  this.style.backgroundColor = "red";
   console.log("外れた時は赤色");
 });
 
 pullDownButton.addEventListener("click", function () {
+  if (pullDownParents.style.display === "block") {
+    pullDownParents.style.display = "none";
+    console.log("非表示");
+  } else {
+    pullDownParents.style.display = "block";
+    console.log("表示");
+  }
+  this.style.backgroundColor = "green";
   console.log("クリックは緑色");
 });
